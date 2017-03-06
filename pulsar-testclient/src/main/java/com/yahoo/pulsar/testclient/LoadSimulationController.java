f5baf640f5bf7b1de44ce64a4206ca6b9064bd8b
breese <breese@yahoo-inc.com>
2017-03-06 10:15:23 -0800

    Add controller and server for simulations

---
+++ pulsar-testclient/src/main/java/com/yahoo/pulsar/testclient/LoadSimulationController.java
@@ -0,0 +1,324 @@
+package com.yahoo.pulsar.testclient;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.List;
+import java.util.Random;
+
+public class LoadSimulationController {
+    private final DataInputStream[] inputStreams;
+    private final DataOutputStream[] outputStreams;
+    private final String[] servers;
+    private final int serverPort;
+    private final String cluster;
+    private final Random random;
+
+    private static class MainArguments {
+        @Parameter(names = {"--cluster"}, description = "Cluster to test on", required = true)
+        String cluster;
+
+        @Parameter(names = {"--servers"}, description = "Comma separated list of server hostnames", required = true)
+        String serverHostNames;
+
+        @Parameter(names = {"--server-port"}, description = "Port that the servers are listening on", required = true)
+        int serverPort;
+    }
+
+    private static class ShellArguments {
+        @Parameter(description = "Command arguments:\n" +
+                "trade tenant namespace topic\n" +
+                "change tenant namespace topic\n" +
+                "stop tenant namespace topic\n" +
+                "trade_group tenant group_name num_namespaces\n" +
+                "change_group tenant group_name\n" +
+                "stop_group tenant group_name\n", required = true)
+        List<String> commandArguments;
+
+        @Parameter(names = {"--rate"}, description = "Messages per second")
+        double rate = 100;
+
+        @Parameter(names = {"--size"}, description = "Message size in bytes")
+        int size = 1024;
+
+        @Parameter(names = {"--separation"}, description = "Separation time in ms for group actions " +
+        "(0 for no separation)")
+        int separation = 0;
+
+        @Parameter(names = {"--topics-per-namespace"}, description = "Number of topics to create per namespace in " +
+        "group actions (total number of topics is num_namespaces X num_topics)")
+        int topicsPerNamespace = 1;
+    }
+
+    public LoadSimulationController(final MainArguments arguments) throws Exception {
+        random = new Random();
+        serverPort = arguments.serverPort;
+        cluster = arguments.cluster;
+        servers = arguments.serverHostNames.split(",");
+        final Socket[] sockets = new Socket[servers.length];
+        inputStreams = new DataInputStream[servers.length];
+        outputStreams = new DataOutputStream[servers.length];
+        System.out.format("Found %d servers:\n", servers.length);
+        for (int i = 0; i < servers.length; ++i) {
+            sockets[i] = new Socket(servers[i], serverPort);
+            inputStreams[i] = new DataInputStream(sockets[i].getInputStream());
+            outputStreams[i] = new DataOutputStream(sockets[i].getOutputStream());
+            System.out.format("Connected to %s\n", servers[i]);
+        }
+    }
+
+    private boolean checkAppArgs(final int numAppArgs, final int numRequired) {
+        if (numAppArgs != numRequired) {
+            System.out.format("ERROR: Wrong number of application arguments (found %d, required %d)\n",
+                    numAppArgs, numRequired);
+            return false;
+        }
+        return true;
+    }
+
+    private String makeDestination(final String tenant, final String namespace, final String topic) {
+        return String.format("persistent://%s/%s/%s/%s", cluster, tenant, namespace, topic);
+    }
+
+    private void writeProducerOptions(final DataOutputStream outputStream, final ShellArguments arguments,
+                                      final String destination)
+            throws Exception {
+        outputStream.writeUTF(destination);
+        outputStream.writeInt(arguments.size);
+        outputStream.writeDouble(arguments.rate);
+    }
+
+    private void trade(final ShellArguments arguments, final String destination) throws Exception {
+        final int i = random.nextInt(servers.length);
+        System.out.println("Sending trade request to " + servers[i]);
+        outputStreams[i].write(LoadSimulationServer.TRADE_COMMAND);
+        writeProducerOptions(outputStreams[i], arguments, destination);
+        outputStreams[i].flush();
+        if (inputStreams[i].read() != -1) {
+            System.out.println("Created producer and consumer for " + destination);
+        } else {
+            System.out.format("ERROR: Socket to %s closed\n", servers[i]);
+        }
+    }
+
+    private void handleTrade(final ShellArguments arguments) throws Exception {
+        final List<String> commandArguments = arguments.commandArguments;
+        if (checkAppArgs(commandArguments.size() - 1, 3)) {
+            final String destination = makeDestination(commandArguments.get(1), commandArguments.get(2),
+                    commandArguments.get(3));
+            trade(arguments, destination);
+        }
+    }
+
+    private void handleChange(final ShellArguments arguments) throws Exception {
+        final List<String> commandArguments = arguments.commandArguments;
+        if (checkAppArgs(commandArguments.size() - 1, 3)) {
+            final String destination = makeDestination(commandArguments.get(1), commandArguments.get(2),
+                    commandArguments.get(3));
+            System.out.println("Searching for server with topic " + destination);
+            for (DataOutputStream outputStream : outputStreams) {
+                outputStream.write(LoadSimulationServer.CHANGE_COMMAND);
+                writeProducerOptions(outputStream, arguments, destination);
+                outputStream.flush();
+            }
+            boolean foundTopic = false;
+            for (int i = 0; i < servers.length; ++i) {
+                int readValue;
+                switch (readValue = inputStreams[i].read()) {
+                    case LoadSimulationServer.FOUND_TOPIC:
+                        System.out.format("Found topic %s on server %s\n", destination, servers[i]);
+                        foundTopic = true;
+                        break;
+                    case LoadSimulationServer.NO_SUCH_TOPIC:
+                        break;
+                    case -1:
+                        System.out.format("ERROR: Socket to %s closed\n", servers[i]);
+                        break;
+                    default:
+                        System.out.println("ERROR: Unknown response signal received: " + readValue);
+                }
+            }
+            if (!foundTopic) {
+                System.out.format("ERROR: Topic %s not found\n", destination);
+            }
+        }
+    }
+
+    private void handleStop(final ShellArguments arguments) throws Exception {
+        final List<String> commandArguments = arguments.commandArguments;
+        if (checkAppArgs(commandArguments.size() - 1, 3)) {
+            final String destination = makeDestination(commandArguments.get(1), commandArguments.get(2),
+                    commandArguments.get(3));
+            System.out.println("Searching for server with topic " + destination);
+            for (DataOutputStream outputStream : outputStreams) {
+                outputStream.write(LoadSimulationServer.STOP_COMMAND);
+                outputStream.writeUTF(destination);
+                outputStream.flush();
+            }
+            boolean foundTopic = false;
+            for (int i = 0; i < servers.length; ++i) {
+                int readValue;
+                switch (readValue = inputStreams[i].read()) {
+                    case LoadSimulationServer.FOUND_TOPIC:
+                        System.out.format("Found topic %s on server %s\n", destination, servers[i]);
+                        foundTopic = true;
+                        break;
+                    case LoadSimulationServer.NO_SUCH_TOPIC:
+                        break;
+                    case LoadSimulationServer.REDUNDANT_COMMAND:
+                        System.out.format("ERROR: Topic %s already stopped on %s\n", destination, servers[i]);
+                        foundTopic = true;
+                        break;
+                    case -1:
+                        System.out.format("ERROR: Socket to %s closed\n", servers[i]);
+                        break;
+                    default:
+                        System.out.println("ERROR: Unknown response signal received: " + readValue);
+                }
+            }
+            if (!foundTopic) {
+                System.out.format("ERROR: Topic %s not found\n", destination);
+            }
+        }
+    }
+
+    private void handleGroupTrade(final ShellArguments arguments) throws Exception {
+        final List<String> commandArguments = arguments.commandArguments;
+        if (checkAppArgs(commandArguments.size() - 1, 3)) {
+            final String tenant = commandArguments.get(1);
+            final String group = commandArguments.get(2);
+            final int numNamespaces = Integer.parseInt(commandArguments.get(3));
+            for (int i = 0; i < numNamespaces; ++i) {
+                for (int j = 0; j < arguments.topicsPerNamespace; ++j) {
+                    final String destination = makeDestination(tenant, String.format("%s-%s", group, i), Integer.toString(j));
+                    trade(arguments, destination);
+                    Thread.sleep(arguments.separation);
+                }
+            }
+        }
+    }
+
+    private void handleGroupChange(final ShellArguments arguments) throws Exception {
+        final List<String> commandArguments = arguments.commandArguments;
+        if (checkAppArgs(commandArguments.size() - 1, 2)) {
+            final String tenant = commandArguments.get(1);
+            final String group = commandArguments.get(2);
+            for (DataOutputStream outputStream: outputStreams) {
+                outputStream.write(LoadSimulationServer.CHANGE_GROUP_COMMAND);
+                outputStream.writeUTF(tenant);
+                outputStream.writeUTF(group);
+                outputStream.writeInt(arguments.size);
+                outputStream.writeDouble(arguments.rate);
+                outputStream.flush();
+            }
+            accumulateAndReport(tenant, group);
+        }
+    }
+
+    private void accumulateAndReport(final String tenant, final String group) throws Exception {
+        int numFound = 0;
+        for (int i = 0; i < servers.length; ++i) {
+            final int foundOnServer = inputStreams[i].readInt();
+            if (foundOnServer == -1) {
+                System.out.format("ERROR: Socket to %s closed\n", servers[i]);
+            } else if (foundOnServer == 0) {
+                System.out.format("Found no topics belonging to tenant %s and group %s on %s\n", tenant, group,
+                        servers[i]);
+            } else if (foundOnServer > 0){
+                System.out.format("Found %d topics belonging to tenant %s and group %s on %s\n", foundOnServer,
+                        tenant, group, servers[i]);
+                numFound += foundOnServer;
+            } else {
+                System.out.format("ERROR: Negative value %d received for topic count on %s\n", foundOnServer,
+                        servers[i]);
+            }
+        }
+        if (numFound == 0) {
+            System.out.format("ERROR: Found no topics belonging to tenant %s and group %s\n", tenant, group);
+        } else {
+            System.out.format("Found %d topics belonging to tenant %s and group %s\n", numFound, tenant, group);
+        }
+    }
+
+    private void handleGroupStop(final ShellArguments arguments) throws Exception {
+        final List<String> commandArguments = arguments.commandArguments;
+        if (checkAppArgs(commandArguments.size() - 1, 2)) {
+            final String tenant = commandArguments.get(0);
+            final String group = commandArguments.get(1);
+            for (DataOutputStream outputStream: outputStreams) {
+                outputStream.write(LoadSimulationServer.STOP_GROUP_COMMAND);
+                outputStream.writeUTF(tenant);
+                outputStream.writeUTF(group);
+                outputStream.flush();
+            }
+            accumulateAndReport(tenant, group);
+        }
+    }
+
+    public void run() throws Exception {
+        final BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            System.out.println();
+            System.out.print("> ");
+            final String[] args = inReader.readLine().split("\\s+");
+            if (args.length > 0 && !(args.length == 1 && args[0].isEmpty())) {
+                final ShellArguments arguments = new ShellArguments();
+                final JCommander jc = new JCommander(arguments);
+                try {
+                    jc.parse(args);
+                    final String command = arguments.commandArguments.get(0);
+                    switch (command) {
+                        case "trade":
+                            handleTrade(arguments);
+                            break;
+                        case "change":
+                            handleChange(arguments);
+                            break;
+                        case "stop":
+                            handleStop(arguments);
+                            break;
+                        case "trade_group":
+                            handleGroupTrade(arguments);
+                            break;
+                        case "change_group":
+                            handleGroupChange(arguments);
+                            break;
+                        case "stop_group":
+                            handleGroupStop(arguments);
+                            break;
+                        case "quit":
+                        case "exit":
+                            System.exit(0);
+                            break;
+                        default:
+                            System.out.format("ERROR: Unknown command \"%s\"\n", command);
+                    }
+                } catch (ParameterException ex) {
+                    ex.printStackTrace();
+                    jc.usage();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        final MainArguments arguments = new MainArguments();
+        final JCommander jc = new JCommander(arguments);
+        try {
+            jc.parse(args);
+        } catch (Exception ex) {
+            jc.usage();
+            ex.printStackTrace();
+            System.exit(1);
+        }
+        (new LoadSimulationController(arguments)).run();
+    }
+}
---
+++ pulsar-testclient/src/main/java/com/yahoo/pulsar/testclient/LoadSimulationServer.java
@@ -0,0 +1,248 @@
+package com.yahoo.pulsar.testclient;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.google.common.util.concurrent.RateLimiter;
+import com.yahoo.pulsar.client.api.*;
+import com.yahoo.pulsar.client.impl.PulsarClientImpl;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.util.concurrent.DefaultThreadFactory;
+import org.apache.commons.lang.SystemUtils;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class LoadSimulationServer {
+    public static final byte FOUND_TOPIC = 0;
+    public static final byte NO_SUCH_TOPIC = 1;
+    public static final byte REDUNDANT_COMMAND = 2;
+    public static final byte CHANGE_COMMAND = 0;
+    public static final byte STOP_COMMAND = 1;
+    public static final byte TRADE_COMMAND = 2;
+    public static final byte CHANGE_GROUP_COMMAND = 3;
+    public static final byte STOP_GROUP_COMMAND = 4;
+    private final ExecutorService executor;
+    private final Map<Integer, byte[]> payloadCache;
+    private final Map<String, TradeUnit> topicsToTradeUnits;
+    private final PulsarClient client;
+    private final ProducerConfiguration producerConf;
+    private final ConsumerConfiguration consumerConf;
+    private final ClientConfiguration clientConf;
+    private final int port;
+
+    private static class TradeUnit {
+        final Future<Producer> producerFuture;
+        final Future<Consumer> consumerFuture;
+        final AtomicBoolean stop;
+        final RateLimiter rateLimiter;
+        final AtomicReference<byte[]> payload;
+        final Map<Integer, byte[]> payloadCache;
+
+        public TradeUnit(final TradeConfiguration tradeConf, final PulsarClient client,
+                         final ProducerConfiguration producerConf, final ConsumerConfiguration consumerConf,
+                         final Map<Integer, byte[]> payloadCache) throws Exception {
+            consumerFuture = client.subscribeAsync(tradeConf.topic, "test-client-subscriber", consumerConf);
+            producerFuture = client.createProducerAsync(tradeConf.topic, producerConf);
+            this.payload = new AtomicReference<>();
+            this.payloadCache = payloadCache;
+            this.payload.set(payloadCache.computeIfAbsent(tradeConf.size, byte[]::new));
+            rateLimiter = RateLimiter.create(tradeConf.rate);
+            stop = new AtomicBoolean(false);
+        }
+
+        public void change(final TradeConfiguration tradeConf) {
+            rateLimiter.setRate(tradeConf.rate);
+            this.payload.set(payloadCache.computeIfAbsent(tradeConf.size, byte[]::new));
+        }
+
+        public void start() throws Exception {
+            final Producer producer = producerFuture.get();
+            final Consumer consumer = consumerFuture.get();
+            while (!stop.get()) {
+                producer.sendAsync(payload.get());
+                rateLimiter.acquire();
+            }
+            producer.closeAsync();
+            consumer.closeAsync();
+        }
+    }
+
+    private static class MainArguments {
+        @Parameter(names = { "-h", "--help" }, description = "Help message", help = true)
+        boolean help;
+
+        @Parameter(names = {"--port"}, description = "Port to listen on for controller", required = true)
+        public int port;
+
+        @Parameter(names = {"--service-url" }, description = "Pulsar Service URL", required = true)
+        public String serviceURL;
+    }
+
+    private static class TradeConfiguration {
+        public byte command;
+        public String topic;
+        public double rate;
+        public int size;
+        public String tenant;
+        public String group;
+        public TradeConfiguration() {
+            command = -1;
+            rate = 100;
+            size = 1024;
+        }
+    }
+
+    private void handle(final Socket socket) throws Exception {
+        final DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+        int command;
+        while ((command = inputStream.read()) != -1) {
+            handle((byte) command, inputStream, new DataOutputStream(socket.getOutputStream()));
+        }
+    }
+
+    private void decodeProducerOptions(final TradeConfiguration tradeConf, final DataInputStream inputStream)
+            throws Exception {
+        tradeConf.topic = inputStream.readUTF();
+        tradeConf.size = inputStream.readInt();
+        tradeConf.rate = inputStream.readDouble();
+    }
+
+    private void decodeGroupOptions(final TradeConfiguration tradeConf, final DataInputStream inputStream)
+        throws Exception {
+        tradeConf.tenant = inputStream.readUTF();
+        tradeConf.group = inputStream.readUTF();
+        tradeConf.size = inputStream.readInt();
+        tradeConf.rate = inputStream.readDouble();
+    }
+
+    private void handle(final byte command, final DataInputStream inputStream, final DataOutputStream outputStream)
+            throws Exception {
+        final TradeConfiguration tradeConf = new TradeConfiguration();
+        tradeConf.command = command;
+        switch(command) {
+            case CHANGE_COMMAND:
+                decodeProducerOptions(tradeConf, inputStream);
+                if (topicsToTradeUnits.containsKey(tradeConf.topic)) {
+                    topicsToTradeUnits.get(tradeConf.topic).change(tradeConf);
+                    outputStream.write(FOUND_TOPIC);
+                } else {
+                    outputStream.write(NO_SUCH_TOPIC);
+                }
+                break;
+            case STOP_COMMAND:
+                tradeConf.topic = inputStream.readUTF();
+                if (topicsToTradeUnits.containsKey(tradeConf.topic)) {
+                    final boolean wasStopped = topicsToTradeUnits.get(tradeConf.topic).stop.getAndSet(true);
+                    outputStream.write(wasStopped ? REDUNDANT_COMMAND: FOUND_TOPIC);
+                } else {
+                    outputStream.write(NO_SUCH_TOPIC);
+                }
+                break;
+            case TRADE_COMMAND:
+                decodeProducerOptions(tradeConf, inputStream);
+                final TradeUnit tradeUnit = new TradeUnit(tradeConf, client, producerConf, consumerConf, payloadCache);
+                topicsToTradeUnits.put(tradeConf.topic, tradeUnit);
+                executor.submit(() -> {
+                    try {
+                        tradeUnit.start();
+                    } catch (Exception ex) {
+                        throw new RuntimeException(ex);
+                    }
+                });
+                outputStream.write(NO_SUCH_TOPIC);
+                break;
+            case CHANGE_GROUP_COMMAND:
+                decodeGroupOptions(tradeConf, inputStream);
+                final String groupRegex = ".*://.*/" + tradeConf.tenant + "/" + tradeConf.group + "-.*/.*";
+                int numFound = 0;
+                for (Map.Entry<String, TradeUnit> entry: topicsToTradeUnits.entrySet()) {
+                    final String destination = entry.getKey();
+                    final TradeUnit unit = entry.getValue();
+                    if (destination.matches(groupRegex)) {
+                        ++numFound;
+                        unit.change(tradeConf);
+                    }
+                }
+                outputStream.writeInt(numFound);
+                break;
+            case STOP_GROUP_COMMAND:
+                decodeGroupOptions(tradeConf, inputStream);
+                final String regex = ".*://.*/.*/" + tradeConf.group + "-.*/.*";
+                int numStopped = 0;
+                for (Map.Entry<String, TradeUnit> entry: topicsToTradeUnits.entrySet()) {
+                    final String destination = entry.getKey();
+                    final TradeUnit unit = entry.getValue();
+                    if (destination.matches(regex) && unit.stop.getAndSet(true)) {
+                        ++numStopped;
+                    }
+                }
+                outputStream.writeInt(numStopped);
+                break;
+            default:
+                throw new IllegalArgumentException("Unrecognized command code received: " + command);
+        }
+        outputStream.flush();
+    }
+
+    private static final MessageListener ackListener = Consumer::acknowledgeAsync;
+
+    public LoadSimulationServer(final MainArguments arguments) throws Exception {
+        payloadCache = new ConcurrentHashMap<>();
+        topicsToTradeUnits = new ConcurrentHashMap<>();
+        final EventLoopGroup eventLoopGroup = SystemUtils.IS_OS_LINUX ?
+                new EpollEventLoopGroup(Runtime.getRuntime().availableProcessors(),
+                        new DefaultThreadFactory("pulsar-test-client")):
+                new NioEventLoopGroup(Runtime.getRuntime().availableProcessors(),
+                        new DefaultThreadFactory("pulsar-test-client"));
+        clientConf = new ClientConfiguration();
+        clientConf.setConnectionsPerBroker(0);
+        producerConf = new ProducerConfiguration();
+        producerConf.setSendTimeout(0, TimeUnit.SECONDS);
+        producerConf.setMessageRoutingMode(ProducerConfiguration.MessageRoutingMode.RoundRobinPartition);
+        producerConf.setBatchingMaxPublishDelay(1, TimeUnit.MILLISECONDS);
+        producerConf.setBatchingEnabled(true);
+        consumerConf = new ConsumerConfiguration();
+        consumerConf.setMessageListener(ackListener);
+        client = new PulsarClientImpl(arguments.serviceURL, clientConf, eventLoopGroup);
+        port = arguments.port;
+        executor = Executors.newCachedThreadPool(new DefaultThreadFactory("test-client"));
+    }
+
+    public static void main(String[] args) throws Exception {
+        final MainArguments mainArguments = new MainArguments();
+        final JCommander jc = new JCommander(mainArguments);
+        try {
+            jc.parse(args);
+        } catch (ParameterException e) {
+            jc.usage();
+            throw e;
+        }
+        (new LoadSimulationServer(mainArguments)).run();
+    }
+
+    public void run() throws Exception {
+        final ServerSocket serverSocket = new ServerSocket(port);
+
+        while (true) {
+            System.out.println("Listening for controller command...");
+            final Socket socket = serverSocket.accept();
+            System.out.format("Connected to %s\n", socket.getInetAddress().getHostName());
+            executor.submit(() -> {
+                try {
+                    handle(socket);
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+            });
+        }
+    }
+}
