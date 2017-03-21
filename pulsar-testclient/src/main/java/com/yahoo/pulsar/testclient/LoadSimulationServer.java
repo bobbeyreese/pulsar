b749fec302e4c01e255b0e725b20c2457c9e1c10
breese <breese@yahoo-inc.com>
2017-03-21 13:04:03 -0700

    Add comments to LoadSimulationServer

--- pulsar-testclient/src/main/java/com/yahoo/pulsar/testclient/LoadSimulationServer.java
+++ pulsar-testclient/src/main/java/com/yahoo/pulsar/testclient/LoadSimulationServer.java
@@ -23,15 +23,24 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 
+/**
+ * LoadSimulationServer is used to simulate client load by maintaining producers and consumers for topics.
+ * Instances of this class are controlled across a network via LoadSimulationController.
+ */
 public class LoadSimulationServer {
+    // Values for command responses.
     public static final byte FOUND_TOPIC = 0;
     public static final byte NO_SUCH_TOPIC = 1;
     public static final byte REDUNDANT_COMMAND = 2;
+
+    // Values for command encodings.
     public static final byte CHANGE_COMMAND = 0;
     public static final byte STOP_COMMAND = 1;
     public static final byte TRADE_COMMAND = 2;
     public static final byte CHANGE_GROUP_COMMAND = 3;
     public static final byte STOP_GROUP_COMMAND = 4;
+
+
     private final ExecutorService executor;
     private final Map<Integer, byte[]> payloadCache;
     private final Map<String, TradeUnit> topicsToTradeUnits;
@@ -41,11 +50,16 @@ public class LoadSimulationServer {
     private final ClientConfiguration clientConf;
     private final int port;
 
+    // A TradeUnit is a Consumer and Producer pair. The rate of message consumption as well as size may be changed at
+    // any time, and the TradeUnit may also be stopped.
     private static class TradeUnit {
         Future<Producer> producerFuture;
         Future<Consumer> consumerFuture;
         final AtomicBoolean stop;
         final RateLimiter rateLimiter;
+
+        // Creating a byte[] for every message is stressful for a client machine, so in order to ensure that any
+        // message size may be sent/changed while reducing object creation, the byte[] is wrapped in an AtomicReference.
         final AtomicReference<byte[]> payload;
         final ProducerConfiguration producerConf;
         final PulsarClient client;
@@ -62,16 +76,21 @@ public class LoadSimulationServer {
             this.payloadCache = payloadCache;
             this.client = client;
             topic = tradeConf.topic;
+
+            // Add a byte[] of the appropriate size if it is not already present in the cache.
             this.payload.set(payloadCache.computeIfAbsent(tradeConf.size, byte[]::new));
             rateLimiter = RateLimiter.create(tradeConf.rate);
             stop = new AtomicBoolean(false);
         }
 
+        // Change the message rate/size according to the given configuration.
         public void change(final TradeConfiguration tradeConf) {
             rateLimiter.setRate(tradeConf.rate);
             this.payload.set(payloadCache.computeIfAbsent(tradeConf.size, byte[]::new));
         }
 
+        // Attempt to create a Producer indefinitely. Useful for ensuring messages continue to be sent after broker
+        // restarts occur.
         private Producer getNewProducer() throws Exception {
             while (true) {
                 try {
@@ -86,14 +105,15 @@ public class LoadSimulationServer {
             public volatile boolean value = true;
         }
 
-        private void startImpl(Producer producer, final Consumer consumer) throws Exception {
+        public void start() throws Exception {
+            Producer producer = producerFuture.get();
+            final Consumer consumer = consumerFuture.get();
             while (!stop.get()) {
                 final MutableBoolean wellnessFlag = new MutableBoolean();
-                final Function<Throwable, ? extends MessageId> exceptionHandler = new Function<Throwable,MessageId>() {
-                    public MessageId apply(final Throwable e) {
-                        wellnessFlag.value = false;
-                        return null;
-                    }
+                final Function<Throwable, ? extends MessageId> exceptionHandler = e -> {
+                    // Unset the well flag in the case of an exception so we can try to get a new Producer.
+                    wellnessFlag.value = false;
+                    return null;
                 };
                 while (!stop.get() && wellnessFlag.value) {
                     producer.sendAsync(payload.get()).exceptionally(exceptionHandler);
@@ -101,20 +121,17 @@ public class LoadSimulationServer {
                 }
                 producer.closeAsync();
                 if (!stop.get()) {
+                    // The Producer failed due to an exception: attempt to get another producer.
                     producer = getNewProducer();
                 } else {
+                    // We are finished: close the consumer.
                     consumer.closeAsync();
                 }
             }
         }
-
-        public void start() throws Exception {
-            Producer producer = producerFuture.get();
-            Consumer consumer = consumerFuture.get();
-            startImpl(producer, consumer);
-        }
     }
 
+    // JCommander arguments for starting a LoadSimulationServer.
     private static class MainArguments {
         @Parameter(names = { "-h", "--help" }, description = "Help message", help = true)
         boolean help;
@@ -126,6 +143,7 @@ public class LoadSimulationServer {
         public String serviceURL;
     }
 
+    // Configuration class for initializing or modifying TradeUnits.
     private static class TradeConfiguration {
         public byte command;
         public String topic;
@@ -140,6 +158,7 @@ public class LoadSimulationServer {
         }
     }
 
+    // Handle input sent from a controller.
     private void handle(final Socket socket) throws Exception {
         final DataInputStream inputStream = new DataInputStream(socket.getInputStream());
         int command;
@@ -148,6 +167,7 @@ public class LoadSimulationServer {
         }
     }
 
+    // Decode TradeConfiguration fields common for topic creation and modification.
     private void decodeProducerOptions(final TradeConfiguration tradeConf, final DataInputStream inputStream)
             throws Exception {
         tradeConf.topic = inputStream.readUTF();
@@ -155,18 +175,21 @@ public class LoadSimulationServer {
         tradeConf.rate = inputStream.readDouble();
     }
 
+    // Decode TradeConfiguration fields common for group commands.
     private void decodeGroupOptions(final TradeConfiguration tradeConf, final DataInputStream inputStream)
         throws Exception {
         tradeConf.tenant = inputStream.readUTF();
         tradeConf.group = inputStream.readUTF();
     }
 
+    // Handle a command sent from a controller.
     private void handle(final byte command, final DataInputStream inputStream, final DataOutputStream outputStream)
             throws Exception {
         final TradeConfiguration tradeConf = new TradeConfiguration();
         tradeConf.command = command;
         switch(command) {
             case CHANGE_COMMAND:
+                // Change the topic's settings if it exists. Report whether the topic was found on this server.
                 decodeProducerOptions(tradeConf, inputStream);
                 if (topicsToTradeUnits.containsKey(tradeConf.topic)) {
                     topicsToTradeUnits.get(tradeConf.topic).change(tradeConf);
@@ -176,6 +199,7 @@ public class LoadSimulationServer {
                 }
                 break;
             case STOP_COMMAND:
+                // Stop the topic if it exists. Report whether the topic was found, and whether it was already stopped.
                 tradeConf.topic = inputStream.readUTF();
                 if (topicsToTradeUnits.containsKey(tradeConf.topic)) {
                     final boolean wasStopped = topicsToTradeUnits.get(tradeConf.topic).stop.getAndSet(true);
@@ -185,6 +209,7 @@ public class LoadSimulationServer {
                 }
                 break;
             case TRADE_COMMAND:
+                // Create the topic. It is assumed that the topic does not already exist.
                 decodeProducerOptions(tradeConf, inputStream);
                 final TradeUnit tradeUnit = new TradeUnit(tradeConf, client, producerConf, consumerConf, payloadCache);
                 topicsToTradeUnits.put(tradeConf.topic, tradeUnit);
@@ -195,12 +220,15 @@ public class LoadSimulationServer {
                         throw new RuntimeException(ex);
                     }
                 });
+                // Tell controller topic creation is finished.
                 outputStream.write(NO_SUCH_TOPIC);
                 break;
             case CHANGE_GROUP_COMMAND:
+                // Change the settings of all topics belonging to a group. Report the number of topics changed.
                 decodeGroupOptions(tradeConf, inputStream);
                 tradeConf.size = inputStream.readInt();
                 tradeConf.rate = inputStream.readDouble();
+                // See if a topic belongs to this tenant and group using this regex.
                 final String groupRegex = ".*://.*/" + tradeConf.tenant + "/" + tradeConf.group + "-.*/.*";
                 int numFound = 0;
                 for (Map.Entry<String, TradeUnit> entry: topicsToTradeUnits.entrySet()) {
@@ -214,7 +242,9 @@ public class LoadSimulationServer {
                 outputStream.writeInt(numFound);
                 break;
             case STOP_GROUP_COMMAND:
+                // Stop all topics belonging to a group. Report the number of topics stopped.
                 decodeGroupOptions(tradeConf, inputStream);
+                // See if a topic belongs to this tenant and group using this regex.
                 final String regex = ".*://.*/" + tradeConf.tenant + "/" + tradeConf.group + "-.*/.*";
                 int numStopped = 0;
                 for (Map.Entry<String, TradeUnit> entry: topicsToTradeUnits.entrySet()) {
@@ -243,11 +273,21 @@ public class LoadSimulationServer {
                 new NioEventLoopGroup(Runtime.getRuntime().availableProcessors(),
                         new DefaultThreadFactory("pulsar-test-client"));
         clientConf = new ClientConfiguration();
+
+        // Disable connection pooling.
         clientConf.setConnectionsPerBroker(0);
+
+        // Disable stats on the clients to reduce CPU/memory usage.
         clientConf.setStatsInterval(0, TimeUnit.SECONDS);
+
         producerConf = new ProducerConfiguration();
+
+        // Disable timeout.
         producerConf.setSendTimeout(0, TimeUnit.SECONDS);
+
         producerConf.setMessageRoutingMode(ProducerConfiguration.MessageRoutingMode.RoundRobinPartition);
+
+        // Enable batching.
         producerConf.setBatchingMaxPublishDelay(1, TimeUnit.MILLISECONDS);
         producerConf.setBatchingEnabled(true);
         consumerConf = new ConsumerConfiguration();
@@ -273,6 +313,8 @@ public class LoadSimulationServer {
         final ServerSocket serverSocket = new ServerSocket(port);
 
         while (true) {
+            // Technically, two controllers can be connected simultaneously, but non-sequential handling of commands
+            // has not been tested or considered and is not recommended.
             System.out.println("Listening for controller command...");
             final Socket socket = serverSocket.accept();
             System.out.format("Connected to %s\n", socket.getInetAddress().getHostName());
